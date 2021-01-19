90fb8b03c475bc17d58c81f7123b162015642405
Christian Parker <christianparker512@gmail.com>
2021-01-19 13:53:19 -0600

    hellow world servlet exercise

--- src/main/java/DadJokeServlet.java
+++ src/main/java/DadJokeServlet.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class DadJokeServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "DadJokeServlet", urlPatterns = "/dadJoke")
+public class DadJokeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/html");
+    PrintWriter out = resp.getWriter();
+
+//    http://localhost:8080/dadjoke?joke=thirsty
+    String joke = req.getParameter("joke");
+
+    if (joke == null){
+        joke = "Hungry";
+    }
+    out.println("<h1>Hi " + joke + " , I'm Dad!</h1>");
+    }
+
 }
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+//        this was taken directly from the DadJokeServlet from lecture
+        String name = req.getParameter("name");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<h1>Hello, " + name + "World!</h1>");
     }
 }
+
